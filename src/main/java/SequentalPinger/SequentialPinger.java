caf17c0b95d330265a9268927f82da84a2b74866
Magnus West Madsen <cph-mm454@cphbusiness.dk>
2018-12-10 09:02:39 +0100

    mid work commit

--- src/main/java/SequentalPinger/SequentialPinger.java
+++ src/main/java/SequentalPinger/SequentialPinger.java
@@ -4,76 +4,104 @@ package SequentalPinger;
  * Code taken from 
  * http://crunchify.com/how-to-get-ping-status-of-any-http-end-point-in-java/
  */
-
-
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class SequentialPinger {
-     public static void main(String args[]) throws Exception {
- 
-        String[] hostList = { "http://crunchify.com", "http://yahoo.com",
-                "http://www.ebay.com", 
-                "http://google.com",
-                "http://www.example.co", 
-                "https://paypal.com",
-                "http://bing.com/", 
-                "http://techcrunch.com/",
-                "http://mashable.com/", 
-                "http://thenextweb.com/",
-                "http://wordpress.com/", 
-                "http://cphbusiness.dk/",
-                "http://example.com/", 
-                "http://sjsu.edu/",
-                "http://ebay.co.uk/", 
-                "http://google.co.uk/",
-                "http://www.wikipedia.org/",
-                "http://dr.dk",
-                "http://pol.dk",
-                "https://www.google.dk",
-                "http://phoronix.com" ,
-                "http://www.webupd8.org/",
-                "https://studypoint-plaul.rhcloud.com/", 
-                "http://stackoverflow.com",
-                "http://docs.oracle.com",
-                "https://fronter.com",
-                "http://imgur.com/", 
-                "http://www.imagemagick.org"
-                };
- 
-        for (int i = 0; i < hostList.length; i++) {
- 
-            String url = hostList[i];
-            String status = getStatus(url);
- 
-            System.out.println(url + "\t\tStatus:" + status);
+
+    public static void main(String args[]) throws Exception {
+
+        String[] hostList = {"http://crunchify.com", "http://yahoo.com",
+            "http://www.ebay.com",
+            "http://google.com",
+            "http://www.example.co",
+            "https://paypal.com",
+            "http://bing.com/",
+            "http://techcrunch.com/",
+            "http://mashable.com/",
+            "http://thenextweb.com/",
+            "http://wordpress.com/",
+            "http://cphbusiness.dk/",
+            "http://example.com/",
+            "http://sjsu.edu/",
+            "http://ebay.co.uk/",
+            "http://google.co.uk/",
+            "http://www.wikipedia.org/",
+            "http://dr.dk",
+            "http://pol.dk",
+            "https://www.google.dk",
+            "http://phoronix.com",
+            "http://www.webupd8.org/",
+            "https://studypoint-plaul.rhcloud.com/",
+            "http://stackoverflow.com",
+            "http://docs.oracle.com",
+            "https://fronter.com",
+            "http://imgur.com/",
+            "http://www.imagemagick.org"
+        };
+
+        ExecutorService es = Executors.newFixedThreadPool(10);
+
+        List<Future<String>> list = new ArrayList<>();
+
+        for (String url : hostList) {
+
+            Callable<String> callable = new MyCallable(url);
+            Future<String> future = es.submit(callable);
+            list.add(future);
         }
- 
+
+        for (Future<String> fut : list) {
+
+            System.out.println(fut.get());
+        }
+
+        es.shutdown();
+
     }
- 
-    public static String getStatus(String url) throws IOException {
- 
+
+}
+
+class MyCallable implements Callable<String> {
+
+    private final String url;
+
+    public MyCallable(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String call() throws Exception {
         String result = "Error";
         try {
             URL siteURL = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-            connection.setConnectTimeout(10000);
+            connection.setConnectTimeout(1000);
             connection.setRequestMethod("GET");
             connection.connect();
- 
+
             int code = connection.getResponseCode();
-            if (code == 200) 
+            if (code == 200) {
                 result = "Green";
-            if(code==301)
-                result="Redirect";
-        } catch (SocketTimeoutException e) {
+            }
+            if (code == 301) {
+                result = "Redirect";
+            }
+            } catch (SocketTimeoutException e) {
             result = "TIMEOUT";
-        
+
         } catch (Exception e) {
             result = "->Red<-";
         }
-        return result;
+
+        return url + "\t\tStatus:" + result;
     }
+
 }
